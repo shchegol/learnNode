b2d244e927ab4c5a11491dae09c5481f0812c40f
Alexander Shchegol <zelenzoom@gmail.com>
2018-05-09 13:47:49 +0300

    koa / chat / done simple stable version

--- 04/chat/index.js
+++ 04/chat/index.js
@@ -1,47 +1,38 @@
 const Koa = require('koa');
 const app = new Koa();
-
 const config = require('config');
-
 const path = require('path');
 const fs = require('fs');
-
-const handlers = fs.readdirSync(path.join(__dirname, 'handlers')).sort();
-handlers.forEach(handler => {
-  const h = require('./handlers/' + handler);
-  h.init(app);
-});
-
-// // can be split into files too
 const Router = require('koa-router');
-
 const router = new Router();
+const handlers = fs.readdirSync(path.join(__dirname, 'handlers')).sort();
+let clients = [];
 
-router.get('/views', async function(ctx, next) {
-  let count = ctx.session.count || 0;
-  ctx.session.count = ++count;
+handlers.forEach(handler => require('./handlers/' + handler).init(app));
 
-  ctx.body = ctx.render('./templates/index.pug', {
-    user: 'John',
-    count: count
+router.get('/subscribe', async ctx => {
+  ctx.set('Cache-Control', 'no-cache')
+
+  ctx.body = await new Promise((res) => {
+    clients.push(res);
+
+    ctx.res.on('close', function() {
+      clients.splice(clients.indexOf(res), 1);
+      res();
+    });
   });
-});
+})
 
+router.post('/publish', async ctx => {
+  const message = ctx.request.body.message
 
-// параметр ctx.params
-// см. различные варианты https://github.com/pillarjs/path-to-regexp
-//   - по умолчанию 1 элемент пути, можно много *
-//   - по умолчанию обязателен, можно нет ?
-//   - уточнение формы параметра через regexp'ы
-router.get('/user/:user', async function(ctx, next) {
-  ctx.body = "Hello, " + ctx.params.user;
-});
+  if (!message) ctx.throw(400, 'message is required!')
 
-router.get('/', async function(ctx) {
-  ctx.redirect('/views');
+  clients.forEach(client => client(String(message)))
 
-  // ctx.body = '1';
-});
+  clients = [];
+  ctx.body = 'ok'
+})
 
 app.use(router.routes());
 
